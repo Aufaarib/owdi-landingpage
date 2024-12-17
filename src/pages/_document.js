f74d7ec22983f7bd579e784d079c9bf7954518c8
Aufaarib <aufa@nuncorp.id>
2024-12-17 13:07:03 +0700

    add title and favicon head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <head>
+        <title>Owdi</title>
+      </head>
       <Head />
       <body className="antialiased">
         <Main />
